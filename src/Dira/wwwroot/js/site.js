a32773547a4647e183c9175217bfc4dd37c322c9
vasil tatarov <vasiltatarov3@gmail.com>
2021-12-26 12:17:06 +0200

    add mouse hover on cards

--- src/Dira/wwwroot/js/site.js
+++ src/Dira/wwwroot/js/site.js
@@ -1,2 +1,22 @@
-﻿var cards = document.getElementsByClassName('card');
-console.log(cards);
+﻿// mouse hover effect on cards
+
+let cards = document.getElementsByClassName("card");
+
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("mouseover", function (event) {
+        // highlight the mouseover target
+        var parent = event.target.parentNode;
+
+        if (parent.classList.contains('card-body')) {
+            parent = parent.parentNode;
+        }
+
+        parent.style.backgroundColor = "lightgray";
+    }, false);
+
+    cards[i].addEventListener("mouseout", function (event) {
+        // highlight the mouseout target
+        var parent = event.target.parentNode;
+        parent.style.backgroundColor = "";
+    }, false);
+}
